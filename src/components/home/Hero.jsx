1332fbbc6dc4fa21a207093fb75649436f23cfae
imanee11 <imaneelouannane@gmail.com>
2026-01-19 14:11:19 +0100

    add hero section

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { images } from '../../constants';
+import ShinyText from '../animation/ShinyText';
 
 const Hero = () => {
     return (
@@ -10,9 +11,20 @@ const Hero = () => {
                     <div>
                         <p className="text-[32px] sm:text-[40px] md:text-[50px] leading-tight">_Hello! I'm</p>
                         <p className="font-bold uppercase text-[40px] sm:text-[60px] md:text-[80px] leading-tight">imane magada_</p>
-                        {/* <p className="text-[12px] sm:text-[14px] text-right mt-2">
-                            <ShinyText text="I create visuals that bring moments and emotions to life." disabled={false} speed={3} className="custom-class" />
-                        </p> */}
+                        <p className="text-[12px] sm:text-[14px] text-left lg:text-right mt-2">
+                            <ShinyText
+                                text="I create visuals that bring moments and emotions to life."
+                                speed={2}
+                                delay={0}
+                                color="#787878"
+                                shineColor="#ffffff"
+                                spread={120}
+                                direction="left"
+                                yoyo={false}
+                                pauseOnHover={false}
+                                disabled={false}
+                            />
+                        </p>
                     </div>
                 </div>
             </section>
