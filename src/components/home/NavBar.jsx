9d4271ab40c644e7137fa3f6f894e106d1fd2088
imanee11 <imaneelouannane@gmail.com>
2026-01-19 11:47:23 +0100

    first commit

---
+++ src/components/home/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1 className='text-red-500 text-2xl font-bold hover:first-line:bg-red-300'>konnichiwa herooo</h1>
+        </div>
+    );
+};
+
+export default NavBar;
